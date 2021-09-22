98421ee523ea769b5fd050db39cf9a9daf84bb69
Watsapon Kampong <k.watsapon@gmail.com>
2021-09-22 21:07:14 +0700

    feat: update movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,35 +3,94 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 10;
-    [SerializeField] private float jump = 5;
+    [SerializeField] private float maxSpeed = 15;
 
-    private Animator m_Animator;
-    private Rigidbody2D m_Rigidbody2D;
-    private bool m_IsJumping;
-    
+    [Header("Vertical Movement")] public float jumpSpeed = 15f;
+    public float jumpDelay = 0.25f;
+    private float _jumpTimer;
+
+    [Header("Physics")] public float linearDrag = 4f;
+    public float gravity = 1f;
+    public float fallMultiplier = 5f;
+
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+    private bool _isJumping = true;
+    private float _xDirection;
+
     private static readonly int Running = Animator.StringToHash("Running");
     private static readonly int Falling = Animator.StringToHash("Falling");
     private static readonly int Jumping = Animator.StringToHash("Jumping");
 
     private void Start()
     {
-        m_Animator = GetComponent<Animator>();
-        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        HandleRunning();
-        HandleJumping();
-        HandleFalling();
+        _xDirection = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _jumpTimer = Time.time + jumpDelay;
+        }
     }
+
+    private void FixedUpdate()
+    {
+        HandleRunning();
+        
+        if (_jumpTimer > Time.time && !_isJumping)
+        {
+            HandleJumping();
+        }
+
+        ModifyPhysics();
+    }
+
+    void ModifyPhysics()
+    {
+        var velocity = _rigidbody.velocity;
+        var changingDirections = (_xDirection > 0 && velocity.x < 0) || (_xDirection < 0 && velocity.x > 0);
+
+        if (!_isJumping)
+        {
+            if (Mathf.Abs(_xDirection) < 0.4f || changingDirections)
+            {
+                _rigidbody.drag = linearDrag;
+            }
+            else
+            {
+                _rigidbody.drag = 0f;
+            }
+
+            _rigidbody.gravityScale = 0;
+        }
+        else
+        {
+            _rigidbody.gravityScale = gravity;
+            _rigidbody.drag = linearDrag * 0.15f;
+            if (_rigidbody.velocity.y < 0)
+            {
+                _rigidbody.gravityScale = gravity * fallMultiplier;
+            }
+            else if (_rigidbody.velocity.y > 0 && !Input.GetKeyDown(KeyCode.Space))
+            {
+                _rigidbody.gravityScale = gravity * (fallMultiplier / 2);
+            }
+            HandleFalling();
+        }
+    }
+
 
     #region Jumping
 
     private void HandleFalling()
     {
-        var yVelocity = m_Rigidbody2D.velocity.y;
-        
+        var yVelocity = _rigidbody.velocity.y;
+
         if (yVelocity > 0) return;
 
         UpdateFallingAnim(yVelocity < 0);
@@ -39,39 +98,54 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateFallingAnim(bool isFalling)
     {
-        m_Animator.SetBool(Falling, isFalling);
+        _animator.SetBool(Falling, isFalling);
     }
 
     private void HandleJumping()
     {
-        if (!Input.GetKeyDown(KeyCode.Space) || m_IsJumping) return;
-        
-        m_Rigidbody2D.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
-        m_IsJumping = true;
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
+        _rigidbody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        _jumpTimer = 0;
+        _isJumping = true;
         UpdateJumpingAnim();
     }
 
     private void UpdateJumpingAnim()
     {
-        m_Animator.SetBool(Jumping, m_IsJumping);
+        _animator.SetBool(Jumping, _isJumping);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (!other.gameObject.CompareTag("Ground")) return;
-        m_IsJumping = false;
+        _isJumping = false;
         UpdateJumpingAnim();
+        UpdateFallingAnim(false);
     }
-    
+
     #endregion
 
     #region Running
+
     private void HandleRunning()
     {
-        var horizontalAxis = Input.GetAxis("Horizontal");
-        var xAxis = speed * horizontalAxis * Time.deltaTime;
-        transform.Translate(xAxis, 0, 0);
-        FlipSprite(horizontalAxis);
+        if (!_isJumping)
+        {
+            _rigidbody.AddForce(Vector2.right * _xDirection);
+            _rigidbody.velocity = new Vector2(_xDirection * speed, _rigidbody.velocity.y);
+        }
+
+        if (_xDirection == 0 && !_isJumping)
+        {
+            _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+        }
+
+        if (Mathf.Abs(_rigidbody.velocity.x) > maxSpeed)
+        {
+            _rigidbody.velocity = new Vector2(Mathf.Sign(_rigidbody.velocity.x) * maxSpeed, _rigidbody.velocity.y);
+        }
+
+        FlipSprite(_xDirection);
     }
 
     private void FlipSprite(float axis)
@@ -97,9 +171,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void TriggerRunningAnim(bool isRunning)
     {
-        m_Animator.SetBool(Running, isRunning);
+        _animator.SetBool(Running, isRunning);
     }
+
     #endregion
-   
-    
 }
